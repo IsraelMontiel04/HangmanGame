f52f25106394bbe8f32f1a0392e3b4cca5ed9719
Israel Montiel <NARUTO-ONE@LIVE.COM.MX>
2024-11-18 23:02:02 -0600

    first version

---
+++ scripts/wordList.js
@@ -0,0 +1,47 @@
+const wordList = [
+    {
+        word:"platano",
+        hint:"Oro parece, plata no es. Abran las cortinas y verán lo que es"
+    },
+    {
+        word:"cangrejo",
+        hint:"Dos pinzas tengo, hacia atrás camino, de mar o de río en el agua vivo."
+    },
+    {
+        word:"avellana",
+        hint:"Soy ave y soy llana, pero no tengo pico ni alas"
+    },
+    {
+        word:"hipopotamo",
+        hint:"Tengo hipo al decir mi nombre, ¿quién soy?"
+    },
+    {
+        word:"silla",
+        hint:"Todos me quieren para descansar. ¡Si ya te lo he dicho! No lo pienses"
+    },
+    {
+        word:"cuchara",
+        hint:"Sube llena y baja vacía. Si no se da prisa, la sopa se enfría. "
+    },
+    {
+        word:"tijeras",
+        hint:"Seguro que en tu casa está, y si no lo sabes no importa. ¿Pero qué cosa será que cuanto más larga más corta?"
+    },
+    {
+        word:"cebolla",
+        hint:"Me abrigo con paños blancos, luzco blanca cabellera y por causa mía llora hasta la misma cocinera."
+    },
+    {
+        word:"helado",
+        hint:"Siempre tan fresquito en el cucurucho. ¡Qué rico y qué frío! Y nos gusta mucho "
+    },
+    {
+        word:"caracol",
+        hint:"Camino sin tener patas, a cuestas llevo mi casa. Por donde mi cuerpo pasa queda un hilillo de plata."
+    },
+    {
+        word:"pulga",
+        hint:"Si la tienes, la buscas; si no la tienes ni la buscas ni la quieres."
+    },
+    
+]
